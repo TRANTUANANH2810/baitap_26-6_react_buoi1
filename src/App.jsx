3d3d6786b66daaa01851ec0992e197d39df95a66
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-06-25 09:22:32 +0700

    bai ta[ thuc hanh react buoi 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Navigator } from "./nav";
+import { Header } from "./header";
+import { Section } from "./section";
+import { Footer } from "./footer";
+
+function App() {
+  return (
+    <>
+      <Navigator />
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
